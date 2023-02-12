5d80cfa9541aa6b3354fbb46f80c158df387acfa
Nicoowr <nicolas.li@hotmail.fr>
2023-02-12 22:35:27 +0100

    refactor: use saveMarkdownFile from dependencies

--- src/domain/index.ts
+++ src/domain/index.ts
@@ -1,14 +1,13 @@
 import {type Dependencies} from "../dependencies/buildDependencies";
 import {convertVttInputToCues} from "./convertVttInputToCues";
 import {type ConversionOptions} from "./types";
-import {saveMarkdownFile} from "../dependencies/saveMarkdownFile";
 
-// TODO: que reste-t-il à faire dans l'API ?
-//  domaine qui importe du domaine ? dep qui importe des dep?
+// TODO: que peut-on faire dans l'API ?
+//  domaine qui importe du domaine ? dep qui importe des dep? api qui importe des dep ?
 //  conversion en markdown = dep ?
 export const convertYoutubeVideoToMarkdownFileDomain = (dependencies: Dependencies) => async (youtubeUrl: string, options?: ConversionOptions) => {
     const vttSubtitles = await dependencies.fetchSubtitles(youtubeUrl);
     const cues = convertVttInputToCues(dependencies)(vttSubtitles, options);
     const markdownString = dependencies.convertCuesToMarkdown(cues);
-    await saveMarkdownFile(markdownString);
+    await dependencies.saveMarkdownFile(markdownString);
 };
