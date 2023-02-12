a9ec24b99afb74b6772d814b02a40c24eb92523d
Nicoowr <nicolas.li@hotmail.fr>
2023-02-12 23:07:31 +0100

    feat: add chapters handling

--- src/dependencies/fetchSubtitlesWithYoutubeDl.ts
+++ src/dependencies/fetchSubtitlesWithYoutubeDl.ts
@@ -2,22 +2,28 @@ import {promisify} from "util"
 import {exec as execNotPromisified} from "child_process"
 import fs from "fs"
 import glob from "glob";
+import {Chapter} from "../domain/types";
+
 const exec = promisify(execNotPromisified);
 
 const subtitlesDirectory = "scripts/subtitleFiles"
-const findNewestVttFile = (directory: string): string => {
-    return glob.sync(`${directory}/*.vtt`)
+const findNewestVttFile = (directory: string, extension: string): string => {
+    return glob.sync(`${directory}/*.${extension}`)
         .map(name => ({name, ctime: fs.statSync(name).ctime}))
         .sort((a, b) => b.ctime - a.ctime)[0].name
 }
+// TODO: handle chapters extraction, specifically the file fetching part (what happens if there is not chapter, might need an identifier)
 export const fetchSubtitlesWithYoutubeDl = async (
-  youtubeLink: string
-): Promise<string> => {
-    // await exec(`cd ${subtitlesDirectory} && youtube-dl --write-sub --sub-lang en --skip-download ${youtubeLink}`);
-    await exec(`cd ${subtitlesDirectory} && youtube-dl --write-auto-sub --sub-lang en --skip-download --write-info-json ${youtubeLink}`);
+    youtubeLink: string
+): Promise<{ subtitles: string; chapters?: Chapter[] }> => {
+    await exec(`cd ${subtitlesDirectory} && youtube-dl --all-subs --skip-download ${youtubeLink}`);
 
-    const newestVttFile = findNewestVttFile(subtitlesDirectory);
+    const newestVttFile = findNewestVttFile(subtitlesDirectory, "vtt");
+    const newestMetadataFile = findNewestVttFile(subtitlesDirectory, "json");
 
-    return fs.readFileSync(newestVttFile).toString();
+    if (newestMetadataFile) {
+
+    }
+    return {subtitles: fs.readFileSync(newestVttFile).toString(), chapters: };
 };
 
