a8f86cee76e09c837ccdbaacc34e9d70454a92c5
Nicoli <nicolas.li@hotmail.fr>
2023-05-27 22:00:04 +0200

    chore: setting up next project (#4)

--- scripts/test.ts
+++
@@ -1,9 +0,0 @@
-import {
-    convertYoutubeVideoToMarkdownFile
-} from "../src/api/convertYoutubeVideoToMarkdownFile";
-
-const main = () => {
-    convertYoutubeVideoToMarkdownFile("https://www.youtube.com/watch?v=UOUBOFZ3Tos")
-}
-
-main()
